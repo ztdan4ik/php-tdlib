2936bf0eb4f86d20865b741027598126016861ce
info@inde.pp.ua <freelance@euve258247.serverprofi24.com>
2018-11-06 11:52:33 +0200

    Base implementation

---
+++ app/Client.php
@@ -0,0 +1,127 @@
+<?php
+namespace phptd;
+
+/**
+ * Class Client
+ * @package phptd
+ */
+class Client
+{
+    /**
+     * Types
+     */
+    const TYPE_UPDATE_AUTH = 'updateAuthorizationState';
+    const TYPE_UPDATE_TERMS = 'updateTermsOfService';
+
+    /**
+     * States
+     */
+    const AUTH_STATE_PHONE = 'authorizationStateWaitPhoneNumber';
+    const AUTH_STATE_CODE = 'authorizationStateWaitCode';
+    const AUTH_STATE_READY = 'authorizationStateReady';
+
+    /**
+     * @var int
+     */
+    public $log_level = 1;
+
+    /**
+     * @var int
+     */
+    public $timeout = 5;
+
+    /**
+     * @var
+     */
+    private $client;
+
+    /**
+     * Client constructor.
+     * @param array $parameters
+     */
+    public function __construct($parameters = [])
+    {
+        foreach ($parameters as $name => $value)
+            $this->{$name} = $value;
+
+        $this->client = td_json_client_create($this->log_level);
+    }
+
+    /**
+     * @param $parameters \phptd\Parameters;
+     */
+    public function setTdlibParameters($parameters) : void
+    {
+        $this->send('setTdlibParameters', ['parameters' => $parameters->toArray()]);
+    }
+
+    /**
+     * @param string|null $key
+     */
+    public function setDatabaseEncryptionKey($key = null) : void
+    {
+        $this->send('setDatabaseEncryptionKey', ['new_encryption_key' => $key]);
+    }
+
+    /**
+     * @param string $phone
+     */
+    public function setAuthenticationPhoneNumber($phone) : void
+    {
+        $this->send('setAuthenticationPhoneNumber', ['phone_number' => $phone]);
+    }
+
+    /**
+     * @param string $code
+     */
+    public function checkAuthenticationCode($code, $first_name = null, $last_name = null) : void
+    {
+        $this->send('checkAuthenticationCode', ['code' => $code, 'first_name' => $first_name, 'last_name' => $last_name]);
+    }
+
+    /**
+     * @param string $id
+     */
+    public function acceptTermsOfService($id) : void
+    {
+        $this->send('acceptTermsOfService', ['terms_of_service_id' => $id]);
+    }
+
+    /**
+     * @param string $type
+     * @param array $parameters
+     */
+    public function send($type, $parameters = []) : void
+    {
+        $parameters = json_encode(array_merge(['@type' => $type], $parameters));
+        td_json_client_send($this->client, $parameters);
+    }
+
+
+    /**
+     * @param string $type
+     * @param array $parameters
+     * @return string
+     */
+    public function execute($type, $parameters = [])
+    {
+        $parameters = json_encode(array_merge(['@type' => $type], $parameters));
+        return td_json_client_execute($this->client, $parameters);
+    }
+
+    /**
+     * @return string
+     */
+    public function receive()
+    {
+        return td_json_client_receive($this->client, $this->timeout);
+    }
+
+    /**
+     *
+     */
+    public function destroy() : void
+    {
+        td_json_client_func_destroy($this->client);
+    }
+}
---
+++ app/Parameters.php
@@ -0,0 +1,44 @@
+<?php
+namespace phptd;
+
+class Parameters
+{
+    private $use_test_dc = 0;
+    private $database_directory = '/var/tmp/tdlib';
+    private $files_directory = '/var/tmp/tdlib';
+    private $use_file_database = 0;
+    private $use_chat_info_database = 0;
+    private $use_message_database = 0;
+    private $use_secret_chats = 0;
+    private $api_id;
+    private $api_hash;
+    private $system_language_code = 'en';
+    private $device_model;
+    private $system_version;
+    private $application_version = '0.0.7';
+    private $enable_storage_optimizer = 1;
+    private $ignore_file_names = 0;
+
+    public function __construct()
+    {
+        if(is_null($this->device_model))
+            $this->device_model = php_uname('s');
+        if(is_null($this->system_version))
+            $this->system_version = php_uname('v');
+    }
+
+    public function setParameter($name, $value)
+    {
+        $this->{$name} = $value;
+    }
+
+    public function getParameter($name)
+    {
+        return $this->{$name};
+    }
+
+    public function toArray()
+    {
+        return call_user_func('get_object_vars', $this);
+    }
+}
